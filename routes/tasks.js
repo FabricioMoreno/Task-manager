d161af356a53d9e5a26bc242f28b1e08f82a3ef1
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-02-18 22:11:09 -0500

    Rounting in api created

---
+++ routes/tasks.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+const {getAllTasks,getTask,createTask,upadteTask,deleteTask} = require("../controllers/tasks")
+
+router.route("/").get(getAllTasks).post(createTask)
+
+router.route("/:id").get(getTask).patch(upadteTask).delete(deleteTask)
+
+
+module.exports = router
