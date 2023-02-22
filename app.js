cd7282b6b278154b0cf02cb44ceca651de2b2917
Fabricio Moreno <fabricio.morenonaranjo@hotmail.com>
2023-02-21 20:13:33 -0500

    All main logic completed

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require("express")
 const app = express()
+const tasks = require("./routes/tasks")
+const connectDB = require("./db/connect")
+const errorHandlerMiddleware = require("./middlewares/error-handler")
+//Load env variables
+require("dotenv").config()
+
+const port = 5000
+
+//middleware
+app.use(express.static("./public"))
+app.use(express.json())
+
+//Routes
+app.use("/api/v1/tasks",tasks)
+
+app.use(errorHandlerMiddleware)
+
+const start = async()=>{
+    try{
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port,()=>{
+            console.log(`Listening on port ${5000}`)
+        })
+
+    }
+    catch(e){
+        console.log("It is a problem of connection db")
+    }
+}
 
 
-
-app.listen("5000",()=>{
-    console.log("Listening on port 5000")
-})
+start()
